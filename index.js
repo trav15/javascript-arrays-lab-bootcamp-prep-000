e5bf2772923aa4949388ab2e0e9de5139f52964d
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-11 13:11:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,3 +23,8 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens]
 }
+
+function removeLastKitten() {
+  var kittensNew = kittens
+  return kittensNew.pop()
+}
